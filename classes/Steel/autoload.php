1065742f461abbb0be9a7fb45509bba6d27abc8a
Eric Rabil <ericrabil@tregmine.com>
2017-01-02 12:59:34 -0500

    Autoload file for framework

---
+++ classes/Steel/autoload.php
@@ -0,0 +1,8 @@
+<?php
+$dir = dirname(__FILE__);
+require_once $dir.'/Settings.php';
+require_once $dir.'/ArrayMethods.php';
+require_once $dir.'/MVC/MVCBundle.php';
+require_once $dir.'/MVC/MVCIdentifier.php';
+require_once $dir.'/Database/IConnection.php';
+require_once $dir.'/Database/Connection.php';
