e33184cd91b72093c9436446c51c0bdc7d7dbb60
Eric Rabil <ericrabil@tregmine.com>
2017-01-02 12:59:07 -0500

    Created ArrayMethods array for standardized functions

---
+++ classes/Steel/ArrayMethods.php
@@ -0,0 +1,14 @@
+<?php
+namespace Steel;
+
+class ArrayMethods{
+    public static function lastKey($array){
+        end($array);
+        return key($array);
+    }
+    
+    public static function lastValue($array){
+        
+        return end($array);
+    }
+}
