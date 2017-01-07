39946b61bf84fef7936d572dc92f366ab84daba4
Eric Rabil <ericrabil@tregmine.com>
2017-01-07 08:59:02 -0500

    Added IApplication to autoloader

--- classes/Steel/autoload.php
+++ classes/Steel/autoload.php
@@ -2,6 +2,7 @@
 $dir = dirname(__FILE__);
 require_once $dir.'/Settings.php';
 require_once $dir.'/ArrayMethods.php';
+require_once $dir.'/IApplication.php';
 require_once $dir.'/MVC/MVCBundle.php';
 require_once $dir.'/MVC/MVCIdentifier.php';
 require_once $dir.'/Database/IConnection.php';
