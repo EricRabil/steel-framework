9a7932cf5da0ee840f7738157c552f6106b85801
Eric Rabil <ericrabil@tregmine.com>
2017-01-19 21:29:05 -0500

    Changed title in apigen sh

--- apigen.sh
+++ apigen.sh
@@ -1,1 +1,1 @@
-apigen generate --source ./ --destination docs/api/v2 --template-theme bootstrap --title "Steel 1 RC 1"
+apigen generate --source ./ --destination docs/api/v3 --template-theme bootstrap --title "Steel 1 RC 2"
