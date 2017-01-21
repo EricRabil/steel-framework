f20dcde2a16f0aae1975b0eeca529594d120bcfd
Eric Rabil <ericrabil@tregmine.com>
2017-01-20 21:02:41 -0500

    Fixed PDOException not being caught

--- classes/Steel/Database/Connection.php
+++ classes/Steel/Database/Connection.php
@@ -16,7 +16,7 @@ class Connection implements IConnection{
                 $this->conn->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
                 $this->conn->setAttribute ( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
                 $this->conn->exec ( "SET NAMES utf8" );
-            } catch (PDOException $e){
+            } catch (\PDOException $e){
                 die($e);
             }
         }else{
