0b3e183891ebcba76d191bfb1ddf635fe8a05976
Eric Rabil <ericrabil@tregmine.com>
2017-01-20 20:34:07 -0500

    Made the Post Installation screen more appealing :)

--- controllers/PostInstallController.php
+++ controllers/PostInstallController.php
@@ -8,10 +8,27 @@ class PostInstallController implements \Steel\MVC\IController {
     public function __construct(\Steel\MVC\MVCBundle $bundle) {
         $this->bundle = $bundle;
         $this->model = $this->bundle->get_model();
+        $this->model->steel->config['steel']['version-metadata'] = explode('-', preg_replace("/[a-zA-Z]/", "", $this->model->steel->config['steel']['version']));
+        $this->model->steel->config['steel']['typeNice'] = ucwords(str_replace('-', ' ', $this->model->steel->config['steel']['type']));
+        $this->model->steel->config['steel']['typeNiceShorthand'] = "";
+        switch($this->model->steel->config['steel']['type']){
+            case 'release-candidate':
+                $this->model->steel->config['steel']['typeNiceShorthand'] = 'RC';
+                break;
+            case 'canary':
+                $this->model->steel->config['steel']['typeNiceShorthand'] = 'Beta';
+                break;
+        }
     }
 
     public function main($params) {
-
+        $stringbuilder = 'Steel ' . $this->model->steel->config['steel']['version-metadata'][0] . ' ';
+        $stringbuilder .= ($this->model->steel->config['steel']['type'] != 'release') ? $this->model->steel->config['steel']['typeNice'] . ' ' : '';
+        $stringbuilder .= (isset($this->model->steel->config['steel']['version-metadata'][1])) ? $this->model->steel->config['steel']['version-metadata'][1] : '';
+        $this->model->page_title = $stringbuilder;
+        $stringbuilder = $this->model->steel->config['steel']['version-metadata'][0]. ' ';
+        $stringbuilder .= (!empty($this->model->steel->config['steel']['typeNiceShorthand'])) ? $this->model->steel->config['steel']['typeNiceShorthand'] . ' ' : '';
+        $stringbuilder .= (isset($this->model->steel->config['steel']['version-metadata'][1])) ? $this->model->steel->config['steel']['version-metadata'][1] : '';
+        $this->model->navbar_title = $stringbuilder;
     }
-
 }
--- models/PostInstallModel.php
+++ models/PostInstallModel.php
@@ -5,7 +5,9 @@ class PostInstallModel implements \Steel\MVC\IModel {
     public $background;
     public $steel;
 
-    public $page_title = "Steel %s";
+    public $page_title = "Steel";
+    public $navbar_title = "";
+    
     public $context = [];
 
     public function __construct(\Steel\Steel $steel) {
