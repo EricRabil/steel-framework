9bb4ac241ba3283205654f004b055ea5fb423fc6
Eric Rabil <ericrabil@tregmine.com>
2017-01-21 20:55:37 -0500

    Removed no longer used constant

--- classes/Steel/MVC/IView.php
+++ classes/Steel/MVC/IView.php
@@ -4,8 +4,6 @@ namespace Steel\MVC;
 
 interface IView {
 
-    const TEMPLATESDIR = '../templates';
-
     public function __construct(\Steel\MVC\MVCBundle $bundle);
 
     public function render();
