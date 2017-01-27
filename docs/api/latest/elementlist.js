69cb3d91803f409571a8113d40855522e22a429e
Eric Rabil <ericrabil@tregmine.com>
2017-01-27 15:01:03 -0500

    Update API

--- docs/api/latest/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-        ApiGen.elements = [["c", "Steel\\ArrayMethods"], ["c", "Steel\\Database\\Connection"], ["c", "Steel\\Database\\IConnection"], ["c", "Steel\\IApplication"], ["c", "Steel\\MVC\\IController"], ["c", "Steel\\MVC\\IErrorController"], ["c", "Steel\\MVC\\IErrorModel"], ["c", "Steel\\MVC\\IModel"], ["c", "Steel\\MVC\\IView"], ["c", "Steel\\MVC\\MVCBundle"], ["c", "Steel\\MVC\\MVCIdentifier"], ["c", "Steel\\Settings"], ["c", "Steel\\Steel"]];
